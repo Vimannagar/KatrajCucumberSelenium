6e297cccea66dcbff93ad9706a740262d533a3eb
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-12-07 20:12:41 +0530

    First commit for Cucumber + selenium

---
+++ src/main/java/KatrajCucucmberSelenium/KatrajCucucmberSelenium/App.java
@@ -0,0 +1,13 @@
+package KatrajCucucmberSelenium.KatrajCucucmberSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
